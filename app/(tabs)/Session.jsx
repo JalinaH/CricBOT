df7b9198f55a6d3cf1dfcc6e1c6c2ac36d91c553
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-02 17:27:46 +0530

    feat: Add sign-in, sign-up, home, profile, schedule, and session components

---
+++ app/(tabs)/Session.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Session = () => {
+  return (
+    <View>
+      <Text>Session</Text>
+    </View>
+  )
+}
+
+export default Session
