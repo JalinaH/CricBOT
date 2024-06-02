0165c7f7803b6af9589813e32d1b9a898eb8cf19
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-02 23:08:56 +0530

    feat: Add constants for icons and images

---
+++ constants/icons.js
@@ -0,0 +1,11 @@
+import home from "../assets/icons/home.png";
+import schedule from "../assets/icons/schedule.png";
+import profile from "../assets/icons/profile.png";
+import session from "../assets/icons/session.png";
+
+export default {
+  home,
+  schedule,
+  profile,
+  session,
+};
