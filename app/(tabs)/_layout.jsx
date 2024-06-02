cd128979fd87bda8a879c220c6cb80a5c83f31c3
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-02 17:47:06 +0530

    chore: Update TabLayout component with navigation tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,51 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Tabs } from "expo-router";
 
 const TabLayout = () => {
   return (
-    <View>
-      <Text>TabLayout</Text>
-    </View>
+    <Tabs>
+      <Tabs.Screen
+        name="Home"
+        options={{
+          title: "Home",
+          //   headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Text style={{ color }}>{focused ? "🏠" : "🏠"}</Text>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          title: "Profile",
+          //   headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Text style={{ color }}>{focused ? "👤" : "👤"}</Text>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Session"
+        options={{
+          title: "Settings",
+          //   headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Text style={{ color }}>{focused ? "⚙️" : "⚙️"}</Text>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Schedule"
+        options={{
+          title: "Settings",
+          //   headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Text style={{ color }}>{focused ? "⚙️" : "⚙️"}</Text>
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
