2bda40bcedeeee0999a4aa9411841b84ae71ed7c
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-03 18:12:54 +0530

    chore: Update Tailwind CSS configuration and add new font families and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+      colors: {
+        primary: "#1C2120",
+        grey: {
+          100: "#8F8E8E",
+          200: "#4D4C4C",
+        },
+        black: "#000",
+        white: "#fff",
+        yellow: "#FFDE59",
+      },
+    },
   },
   plugins: [],
-}
-
+};
