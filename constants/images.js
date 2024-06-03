a7d31895039969cfaf47a17ab905c44aa4f11baa
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-03 18:03:49 +0530

    feat: Add new images for bouncer, yorker, slow, fast, swing, random, beginner, and pro

--- constants/images.js
+++ constants/images.js
@@ -1,4 +1,12 @@
 import logo from "../assets/logo.png";
+import bouncer from "../assets/bouncer.png";
+import yorker from "../assets/yorker.png";
+import slow from "../assets/slow.png";
+import fast from "../assets/fast.png";
+import swing from "../assets/swing.png";
+import random from "../assets/random.png";
+import beginner from "../assets/beginner.png";
+import pro from "../assets/pro.png";
 
 export default {
   logo,
