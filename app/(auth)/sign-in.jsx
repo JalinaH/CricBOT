30f32f38c6be848b478d1262c10bd12d79fd90bf
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-04 23:35:24 +0530

    feat: Update Signin component with Expo Router and SignUp link
    - Replace react-native View and Text components with Expo Router Stack and Screen components in AuthLayout
    - Add className "m-auto" to Signin component in sign-in.jsx
    - Add SignUp link for users who don't have an account

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -5,12 +5,13 @@ import images from '../../constants/images'
 import FormField from '../../components/FormField';
 import { StatusBar } from 'expo-status-bar';
 import CustomButton from '../../components/CustomButton';
+import { Link } from 'expo-router';
 
 const Signin = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <View className="bg-white h-[120px] w-[120px] rounded-xl mx-auto mt-[110px] mb-20">
-        <Image 
+        <Image
           source={images.logo}
           className="w-[100px] h-[100px] justify-center mx-auto my-auto"
           resizeMode="contain"
@@ -22,23 +23,29 @@ const Signin = () => {
           Log in to CricBOT
         </Text>
 
-        <FormField 
+        <FormField
           title={"Email"}
           placeholder={"Enter your email"}
           otherStyles={"mt-5"}
         />
 
-        <FormField 
-          title={"Password"}
-          placeholder={"Enter your password"}
-          
-        />
+        <FormField title={"Password"} placeholder={"Enter your password"} />
 
-        <CustomButton 
+        <CustomButton
           title={"Sign In"}
           handlePress={() => {}}
           containerStyles={"mt-[50px] w-[80%] mx-auto"}
         />
+
+        <View className="flex flex-row justify-center mt-5">
+          <Text className="font-pmedium text-black">
+            Don't have an account?
+          </Text>
+          <Link href='sign-up' className='font-psemibold'> SignUp</Link>
+        </View>
+        <View className="flex flex-row justify-center mt-1">
+          <Text className="font-pmedium text-black">Forgot your password?</Text>
+        </View>
       </View>
 
       <StatusBar style="light" backgroundColor="#1C2120" />
