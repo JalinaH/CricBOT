d871483960711bdbd3be1a299adfa4ffdc4f4e9c
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-04 13:24:58 +0530

    feat: Update Signin component with SafeAreaView and image logo
    - Add SafeAreaView component to wrap the Signin component for better layout
    - Add image logo to display the CricBOT logo in the Signin component

---
+++ components/FormField.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FormField = () => {
+  return (
+    <View>
+      <Text>FormField</Text>
+    </View>
+  );
+};
+
+export default FormField;
