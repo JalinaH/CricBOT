34906c446fa049564655755f05a7a2425b41c942
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-04 23:27:12 +0530

    Update FormField component with placeholder text color

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -13,11 +13,12 @@ const FormField = ({
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base font-pmedium text-grey-200 mt-7 pl-[45px]">{title}</Text>
 
-      <View className="w-[80%] h-16 px-4 bg-grey-100 border-2 border-gray-600 rounded-2xl mx-auto focus:border-primary">
+      <View className="w-[80%] h-16 px-4 bg-gray-300 border-2 border-gray-600 rounded-2xl mx-auto focus:border-primary">
         <TextInput
           className="flex-1 font-psemibold text-grey-200 text-base"
           value={value}
           placeholder={placeholder}
+          placeholderTextColor={"#4D4C4C"}
           onChangeText={handleChangeText}
           {...props}
         />
