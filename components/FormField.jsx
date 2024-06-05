356c1798a02541285f1375ad6091fedf246569b3
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-05 13:53:41 +0530

    feat: Update FormField component with secureTextEntry for password field
    - Add useState hook to manage showPassword state
    - Set secureTextEntry prop to true for password field when showPassword is false

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,5 +1,5 @@
 import { View, Text, TextInput } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 const FormField = ({
   title,
@@ -9,9 +9,14 @@ const FormField = ({
   otherStyles,
   ...props
 }) => {
+
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <View className={`space-y-2 ${otherStyles}`}>
-      <Text className="text-base font-pmedium text-black mt-7 pl-[45px]">{title}</Text>
+      <Text className="text-base font-pmedium text-black mt-7 pl-[45px]">
+        {title}
+      </Text>
 
       <View className="w-[80%] h-16 px-4 bg-gray-300 border-2 border-gray-600 rounded-2xl mx-auto focus:border-primary">
         <TextInput
@@ -20,6 +25,7 @@ const FormField = ({
           placeholder={placeholder}
           placeholderTextColor={"#4D4C4C"}
           onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
           {...props}
         />
       </View>
