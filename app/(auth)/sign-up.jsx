f0386216b2bdaf4cbc91d2f59be3baae725edd01
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-05 13:39:05 +0530

    feat: Update Signin component with ScrollView and SignUp link
    - Add ScrollView component to wrap the Signin component for better scrolling experience
    - Add SignUp link for users who don't have an account

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image, ScrollView } from "react-native";
+import React from "react";
 
-const Signup = () => {
+import images from "../../constants/images";
+import FormField from "../../components/FormField";
+import { StatusBar } from "expo-status-bar";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
+
+const Signin = () => {
   return (
-    <View>
-      <Text>Signup</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="bg-white h-[90vh] rounded-3xl mt-[20vh]">
+          <Text className="font-pbold text-3xl text-center mt-10">
+            Sign Up to CricBOT
+          </Text>
+          <View className="">
+            <Text className="font-pregular text-center text-m text-grey-200 mt-[10px]">
+              Already have an account?{" "}
+              <Link href="sign-in" className="font-psemibold">
+                Sign In
+              </Link>
+            </Text>
+          </View>
 
-export default Signup
+          <FormField
+            title={"Username"}
+            placeholder={"Enter your username"}
+            otherStyles={"mt-5"}
+          />
+          <FormField title={"Email"} placeholder={"Enter your email"} />
+
+          <FormField title={"Password"} placeholder={"Enter your password"} />
+
+          <CustomButton
+            title={"Sign Up"}
+            handlePress={() => {}}
+            containerStyles={"mt-[50px] w-[80%] mx-auto"}
+          />
+        </View>
+
+        <StatusBar style="light" backgroundColor="#1C2120" />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Signin;
