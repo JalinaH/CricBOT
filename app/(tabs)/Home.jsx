e2d876ccca23ffdcc144206b7e111dbb23d4210a
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-17 11:05:44 +0530

    feat: Add statistics section to Home page
    The code changes include adding a statistics section to the Home page, displaying the number of sessions, balls, and minutes. This enhancement improves the user experience by providing relevant information at a glance.

--- app/(tabs)/Home.jsx
+++ app/(tabs)/Home.jsx
@@ -24,6 +24,21 @@ const Home = () => {
             </View>
           </View>
 
+          <View className="mt-10 flex flex-row w-[100%] mx-auto">
+            <View className="flex flex-col w-[33%] items-center">
+              <Text className="font-psemibold text-lg">2</Text>
+              <Text className="text-grey-100">Sessions</Text>
+            </View>
+            <View className="flex flex-col w-[33%] items-center">
+              <Text className="font-psemibold text-lg">100</Text>
+              <Text className="text-grey-100">Balls</Text>
+            </View>
+            <View className="flex flex-col w-[33%] items-center">
+              <Text className="font-psemibold text-lg">160</Text>
+              <Text className="text-grey-100">Minutes</Text>
+            </View>
+          </View>
+
           <StatusBar style="light" backgroundColor="#1C2120" />
         </ScrollView>
       </SafeAreaView>
