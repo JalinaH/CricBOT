7db3c1f90848707c91f5d3134c2051197dda888a
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-17 10:51:20 +0530

    feat: Add ScrollView and SafeAreaView to Home component

--- app/(tabs)/Home.jsx
+++ app/(tabs)/Home.jsx
@@ -1,23 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { Image } from "react-native";
 import images from "../../constants/images";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <View>
-      <View className="flex flex-row items-center mt-5">
-        <Image
-          source={images.logo}
-          className="w-20 h-20 justify-center mt-5"
-          resizeMode="contain"
-        />
-        <View className="mt-7">
-          <Text className="text-xl font-psemibold">CricBOT</Text>
-          <Text className="text-lg">Smart Cricket Trainer</Text>
-        </View>
-      </View>
-    </View>
+    <>
+      <SafeAreaView>
+        <ScrollView>
+          <View>
+            <View className="flex flex-row items-center mt-5">
+              <Image
+                source={images.logo}
+                className="w-20 h-20 justify-center mt-5"
+                resizeMode="contain"
+              />
+              <View className="mt-7">
+                <Text className="text-xl font-psemibold">CricBOT</Text>
+                <Text className="text-lg">Smart Cricket Trainer</Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };
 
