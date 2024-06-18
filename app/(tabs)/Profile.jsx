37a464b2a3a59c63723526071205c6219e4084c6
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-18 16:26:49 +0530

    feat: Update tab names and icons in TabLayout component
    The code changes update the names and icons of the tabs in the TabLayout component. These changes improve the clarity and consistency of the tab names and icons in the TabLayout component.

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const Profile = () => {
   return (
     <View>
       <Text>Profile</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
