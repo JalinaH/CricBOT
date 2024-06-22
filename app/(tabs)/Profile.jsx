6f155cc195247c2c6fde9c712d6bbfd1409807e9
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-22 12:10:21 +0530

    feat: Update Profile and Schedule components to use SafeAreaView and ScrollView

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,12 +1,32 @@
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
+import { UserCircle } from "phosphor-react-native";
 
 const Profile = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <View>
-          
+        <View className="">
+          <View className="mt-20 items-center">
+            <UserCircle size={150} />
+            <View className="justify-center items-center">
+              <Text className="font-psemibold text-lg">Jalina Hirushan</Text>
+              <Text className="font-plight text-[12px] text-grey-100">
+                jalinahirushan2002@gmail.com
+              </Text>
+            </View>
+          </View>
+
+          <View>
+            <View>
+              <Text className="font-pbold text-lg mt-10 ml-5">Account</Text>
+              <View className="mt-5">
+                <Text className="font-psemibold ml-5">Edit Profile</Text>
+                <Text className="font-psemibold ml-5">Change Password</Text>
+                <Text className="font-psemibold ml-5">Logout</Text>
+              </View>
+            </View>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
--- app/(tabs)/Schedule.jsx
+++ app/(tabs)/Schedule.jsx
@@ -5,7 +5,7 @@ const Schedule = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <View className="m-auto">
+        <View className="h-[100vh] items-center justify-center">
           <Text className="font-pbold text-3xl">Under Development!</Text>
         </View>
       </ScrollView>
