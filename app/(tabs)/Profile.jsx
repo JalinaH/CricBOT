2950787b30c6904b9796c4cf2d07aa6d695a32a7
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-22 11:39:06 +0530

    refactor: Update Profile and Schedule components to use SafeAreaView and ScrollView
    The code changes in the Profile and Schedule components update the layout to use SafeAreaView and ScrollView components. This ensures that the content is displayed correctly and allows for scrolling when necessary. The update improves the overall user experience and follows the repository's guidelines.

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,12 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-      <Text>Hi</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
