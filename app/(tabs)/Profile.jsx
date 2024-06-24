0212220f4cc2ecdda80848ba9f8b6ff44335676d
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-24 15:40:59 +0530

    feat: Add account and support sections to Profile component
    The code changes in the Profile component add separate sections for account and support. Each section includes relevant options such as editing the profile, changing the password, and accessing support resources. This update improves the organization and usability of the Profile component.

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,6 +1,14 @@
-import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
-import { UserCircle } from "phosphor-react-native";
+import { UserCircle, Question, Info, AddressBook } from "phosphor-react-native";
+import { PencilSimple, ShareNetwork } from "phosphor-react-native";
+import { BellSimple, Key } from "phosphor-react-native";
 
 const Profile = () => {
   return (
@@ -19,11 +27,67 @@ const Profile = () => {
 
           <View>
             <View>
-              <Text className="font-pbold text-lg mt-10 ml-5">Account</Text>
-              <View className="mt-5">
-                <Text className="font-psemibold ml-5">Edit Profile</Text>
-                <Text className="font-psemibold ml-5">Change Password</Text>
-                <Text className="font-psemibold ml-5">Logout</Text>
+              <View className="h-10 w-[100%] bg-[#EDEDED] justify-center mt-10 ">
+                <Text className="font-pregular ml-5 text-grey-100 text-lg">
+                  Account
+                </Text>
+              </View>
+
+              <View className="mt-5 ml-5">
+                <TouchableOpacity>
+                  <View className="flex flex-row mb-2 items-center">
+                    <PencilSimple size={18} className="mr-2" />
+                    <Text className="font-pregular text-sm">Edit Profile</Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <View className="flex flex-row mb-2 items-center">
+                    <Key size={18} className="mr-2" />
+                    <Text className="font-pregular text-sm">
+                      Change Password
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <View className="flex flex-row mb-2 items-center">
+                    <BellSimple size={18} className="mr-2" />
+                    <Text className="font-pregular text-sm">Notification</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+            
+            <View>
+              <View className="h-10 w-[100%] bg-[#EDEDED] justify-center mt-10 ">
+                <Text className="font-pregular ml-5 text-grey-100 text-lg">
+                  Support
+                </Text>
+              </View>
+              <View className="mt-5 ml-5">
+                <TouchableOpacity>
+                  <View className="flex flex-row mb-2 items-center">
+                    <Question size={18} className="mr-2" />
+                    <Text className="font-pregular text-sm">Help</Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <View className="flex flex-row mb-2 items-center">
+                    <AddressBook size={18} className="mr-2" />
+                    <Text className="font-pregular text-sm">Contact Us</Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <View className="flex flex-row mb-2 items-center">
+                    <Info size={18} className="mr-2" />
+                    <Text className="font-pregular text-sm">About Us</Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <View className="flex flex-row mb-2 items-center">
+                    <ShareNetwork size={18} className="mr-2" />
+                    <Text className="font-pregular text-sm">Share App</Text>
+                  </View>
+                </TouchableOpacity>
               </View>
             </View>
           </View>
