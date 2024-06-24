d62b0a56bc3518a1c0699d30166943c5f8a847a3
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-24 15:58:09 +0530

    feat: Update Session component to handle missing title parameter

---
+++ app/(tabs)/Stats.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Stats = () => {
+  return (
+    <View>
+      <Text>Stats</Text>
+    </View>
+  )
+}
+
+export default Stats
